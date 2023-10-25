1d63d4d8370e03a60790beee05384dea262f01e9
Aniketh Galla <orbitgum68@gmail.com>
2023-10-25 22:09:13 +0530

    Fixes and enhancements.

--- onlineclassroom.py
+++ onlineclassroom.py
@@ -16,6 +16,9 @@ df = pd.DataFrame(data)
 
 # Set the page title
 st.title("Online Classroom Links")
+st.write("""
+For successful online classes, prepare in advance, access class links, engage actively, test equipment, ask questions, and minimize distractions. Take notes, participate in discussions, and stay motivated. 
+Connect with peers, provide feedback for improvement, and stay organized to make the most of your virtual learning experience.""")
 
 # Display the DataFrame in a Streamlit table
 st.dataframe(df, width=600, height=300)
--- website.py
+++ website.py
@@ -6,6 +6,7 @@ url_resources = "https://congnixresources.streamlit.app/"
 url_notes = "https://congnixnotes.streamlit.app/"
 url_physicslab="https://www.physicsclassroom.com/"
 url_chemsirtlab="http://chemcollective.org/"
+url_onlineclassrooms="https://cognixclassrooms.streamlit.app/"
 # Create the Streamlit web app
 st.set_page_config(
     page_title="Simple Website",
@@ -21,6 +22,7 @@ st.sidebar.link_button("Student resources", url_resources)
 st.sidebar.link_button("Notes", url_notes)
 st.sidebar.link_button("Physics lab", url_physicslab)
 st.sidebar.link_button("Chemistry lab", url_chemsirtlab)
+st.sidebar.link_button("Online Classrooms", url_onlineclassrooms)
 
 # Add more content as needed
 st.write("""Hello student I am Cognix your ai mentor.How may I help you today.\n
